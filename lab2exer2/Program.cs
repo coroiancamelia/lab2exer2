ec2bbed483de1ea5e7cf0ed9e7c7059a9356605a
coroiancamelia <coroiancamelia@outlook.com>
2022-12-07 13:40:26 +0200

    Add project files.

---
+++ lab2exer2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab2exer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Ex 2: Scrieti un program care va afisa factorialul unui numar n, n fiind citit
+           de la tastatura.*/
+
+            Console.WriteLine("Introduceti un numar:");
+
+            int n = int.Parse(Console.ReadLine());
+            int f = 1;
+
+            while (n > 0)
+            {
+                f = f * n;
+                n = n - 1;
+            }
+            
+
+
+
+
+           Console.WriteLine("Numarul este: " + f);
+        }
+    }
+}
